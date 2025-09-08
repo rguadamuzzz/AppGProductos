adee5beafe43262e8894174a78782df8449c5e42
rguadamuzzz <guadamuz.rosa@gmail.com>
2025-09-08 17:04:31 -0500

    Actualización

--- FrontEnd/app-gestion/.next/types/app/register/page.ts
+++ FrontEnd/app-gestion/.next/types/app/register/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\probook\Music\APPGestion\FrontEnd\app-gestion\src\app\register\page.tsx
+// File: C:\Users\probook\Videos\AppGProductos\FrontEnd\app-gestion\src\app\register\page.tsx
 import * as entry from '../../../../src/app/register/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
